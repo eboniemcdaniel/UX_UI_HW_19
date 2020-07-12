792c48bb8878c2c73ccd8ea5c4f142967a392d2b
Ebonie <eboniemcdaniel@gmail.com>
2020-07-11 21:01:04 -0700

    jQuery troubleshooting

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 console.log("Your index.js file works");
 
-// $("li").hover(console.log("Mouse In"),console.log("Mouse Out"));
+$("li").hover(console.log("Mouse In"),console.log("Mouse Out"));
 
 $("li").click(console.log("li Clicked"));
