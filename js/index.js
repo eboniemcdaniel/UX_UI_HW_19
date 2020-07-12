0f3e8f5702dc8a3b5b965867b742cda74dd1eec8
Ebonie <eboniemcdaniel@gmail.com>
2020-07-11 21:15:53 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,12 @@
 console.log("Your index.js file works");
 
-$("li").hover(console.log("Mouse In"),console.log("Mouse Out"));
+// $("li").hover(console.log("Mouse In"),console.log("Mouse Out"));
 
-$("li").click(console.log("li Clicked"));
+// $("li").click(console.log("li Clicked"));
+
+$("li").hover(function (){
+        $(this).css("text-decoration", "underline");
+    },function(){
+        $(this).css("text-decoration", "none");
+    }
+);
